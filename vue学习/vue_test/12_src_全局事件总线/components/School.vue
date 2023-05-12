2676258a15c16fe7f99f08ee78935f0c17c8196f
chengyangguang_zz <chengyg09@chinatelecom.cn>
2023-05-12 17:28:42 +0800

    全局事件总线

---
+++ vue学习/vue_test/12_src_全局事件总线/components/School.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="school">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'School',
+        data() {
+            return {
+                name: '清华大学',
+                address: '郑州'
+            }
+        },
+        mounted() {
+            // console.log('School', this.$bus);
+            this.$bus.$on('hello', (data) => {
+                console.log('我是School组件，收到了数据：', data);
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .school {
+        background-color: skyblue;
+        padding: 5px;
+    }
+</style>
